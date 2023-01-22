f2c5dd8427747d0166c33cb382decbad0aa484f2
Kaiseridze <kaiseidze@gmail.com>
2023-01-22 21:46:31 +0300

    add YMap

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { YMaps, Map } from "@pbe/react-yandex-maps";
 import { Container, Sidebar } from "../../Components";
+
+import "./Home.styles.scss";
 
 const Home = () => {
   return (
     <Container variant="m">
-      <Sidebar />
+      <div className="home">
+        <Sidebar />
+        <YMaps>
+          <div>
+            My awesome application with maps!
+            <Map defaultState={{ center: [59.5619,  30.1850], zoom: 9 }} />
+          </div>
+        </YMaps>
+      </div>
     </Container>
   );
 };
