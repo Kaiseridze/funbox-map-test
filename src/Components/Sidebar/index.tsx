d73c06317897e6247f3447d17b6d71d32baf89b9
Kaiseridze <kaiseidze@gmail.com>
2023-01-22 19:57:03 +0300

    add input

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import { Input } from "../../UI";
 import "./Sidebar.styles.scss";
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="sidebar">
+      <h1 className="sidebar__title">Добавьте новую точку</h1>
+      <form onSubmit={onSubmit}>
+        <Input />
+      </form>
+    </div>
+  );
 };
 
 export default Sidebar;
