5d482161ccf4e63a423e962a7fc78b907e96082c
Kaiseridze <kaiseidze@gmail.com>
2023-01-25 04:31:25 +0300

    add type on handleMapFunction

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { MapEvent } from "yandex-maps";
 import { Container, Sidebar, InteractiveMap } from "../../Components";
 
 import "./Home.styles.scss";
@@ -7,7 +8,7 @@ const Home = () => {
   const defaultCenter = [59.93431286622302, 30.31946594757369];
   const [mapCenter, setMapCenter] = useState(defaultCenter);
 
-  const handleMapMove = (e: any) => {
+  const handleMapMove = (e: MapEvent) => {
     const newCenter = e.get("newCenter");
     setMapCenter(newCenter);
   };
