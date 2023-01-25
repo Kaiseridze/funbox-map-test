51a0a1bccd66bacc9cf22f285ae7c35570f89651
Kaiseridze <kaiseidze@gmail.com>
2023-01-25 03:19:56 +0300

    add types for roadMap

--- src/Entities/RoadMap/RoadMap.types.ts
+++ src/Entities/RoadMap/RoadMap.types.ts
@@ -1,10 +1,14 @@
-import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from "@hello-pangea/dnd";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "@hello-pangea/dnd";
+
 
 export interface IRoadMap {
-    title: string,
-    id: string,
-    onClick?: React.MouseEventHandler<HTMLDivElement>;
-    reference?: any
-    draggableProps?: DraggableProvidedDraggableProps
-    dragHandleProps?: DraggableProvidedDragHandleProps | null
+  title: string;
+  id: string;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+  reference: React.Ref<HTMLLIElement>;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null;
 }
