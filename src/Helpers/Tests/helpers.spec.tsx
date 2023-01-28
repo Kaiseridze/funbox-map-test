46834667640ea904b3ca30b7fd9d321dcb0281a5
Kaiseridze <kaiseidze@gmail.com>
2023-01-28 05:46:24 +0300

    added tests for reorderList function

---
+++ src/Helpers/Tests/helpers.spec.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+import reorderList from "../ReorderList";
+
+test("Reorder list", () => {
+  const startIndex = 0;
+  const endIndex = 3;
+
+  const initArray = [
+    { title: "One", id: "1vxcvdrwer", coordinates: [1, 2] },
+    { title: "Two", id: "12dfs342", coordinates: [1, 2] },
+    { title: "Three", id: "123321", coordinates: [1, 2] },
+  ];
+
+  const expectedArray = [
+    { title: "Two", id: "12dfs342", coordinates: [1, 2] },
+    { title: "Three", id: "123321", coordinates: [1, 2] },
+    { title: "One", id: "1vxcvdrwer", coordinates: [1, 2] },
+  ];
+
+  for (let i = 0; i < expectedArray.length; i++) {
+    expect(reorderList(initArray, startIndex, endIndex)[i]).toMatchObject(
+      expectedArray[i]
+    );
+  }
+});
