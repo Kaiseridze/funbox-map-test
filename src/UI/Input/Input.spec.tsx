b0b6c6a9c1afe0140bd0d7f89487a81dc3ee566d
Kaiseridze <kaiseidze@gmail.com>
2023-01-29 20:55:17 +0300

    refactor Input tests

--- src/UI/Input/Input.spec.tsx
+++ src/UI/Input/Input.spec.tsx
@@ -4,31 +4,33 @@ import "@testing-library/jest-dom";
 import Input from "./index";
 import userEvent from "@testing-library/user-event";
 
-test("Renders Input component", () => {
-  render(<Input onChange={() => {}} value="" />);
-  const input = screen.getByRole<HTMLInputElement>("textbox");
+describe("Input test", () => {
+  it("Renders Input component", () => {
+    render(<Input onChange={() => {}} value="" />);
+    const input = screen.getByRole<HTMLInputElement>("textbox");
 
-  expect(input).toBeInTheDocument();
-});
+    expect(input).toBeInTheDocument();
+  });
 
-test("Renders Input component with value", () => {
-  render(<Input onChange={() => {}} value="testValue" />);
-  const input = screen.getByRole<HTMLInputElement>("textbox");
+  it("Renders Input component with value", () => {
+    render(<Input onChange={() => {}} value="testValue" />);
+    const input = screen.getByRole<HTMLInputElement>("textbox");
 
-  expect(input.value).toBe("testValue");
-});
+    expect(input.value).toBe("testValue");
+  });
 
-test("Renders Input component without value", () => {
-  render(<Input onChange={() => {}} value="" />);
-  const input = screen.getByRole<HTMLInputElement>("textbox");
+  it("Renders Input component without value", () => {
+    render(<Input onChange={() => {}} value="" />);
+    const input = screen.getByRole<HTMLInputElement>("textbox");
 
-  expect(input.value).toBe("");
-});
+    expect(input.value).toBe("");
+  });
 
-test("simulate user input", async () => {
-  render(<Input onChange={() => {}}/>);
-  const input = screen.getByRole<HTMLInputElement>("textbox");
-  await userEvent.type(input, "Hello, World!");
+  it("Simulate user input", async () => {
+    render(<Input onChange={() => {}} />);
+    const input = screen.getByRole<HTMLInputElement>("textbox");
+    await userEvent.type(input, "Hello, World!");
 
-  expect(input).toHaveValue("Hello, World!");
+    expect(input).toHaveValue("Hello, World!");
+  });
 });
